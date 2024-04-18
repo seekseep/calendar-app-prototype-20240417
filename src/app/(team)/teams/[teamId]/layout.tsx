6c2b2a28ef71e60754a37a675299c80291abd542
YOKOYAMA Taku <seekseep@gmail.com>
2024-04-18 14:02:26 +0900

    [FIX] 修正

--- src/app/(team)/teams/[teamId]/layout.tsx
+++ src/app/(team)/teams/[teamId]/layout.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from 'react'
 
-import Navigation from '../components/Navigation'
+import Navigation from '../../components/Navigation'
 
 export default function TeamLayout ({
   children
